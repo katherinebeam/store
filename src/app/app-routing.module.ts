7f8068ac7c1c0f14a66ff4872ffba33c512105e0
Katherine Beam <Katherine.Beam@hyland.com>
2022-03-19 12:53:12 -0400

    initial commit. set up the html for the product list page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/models/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    url: string;
+
+    constructor() {
+        this.id = 1;
+        this.name = '';
+        this.description = '';
+        this.price = 0;
+        this.url = '';
+    }
+}
